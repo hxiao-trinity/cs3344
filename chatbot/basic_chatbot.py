0a2670a0afe3b0b647c8a1b83a2dbc37eac3ccd3
H X <hxiao@trinity.edu>
2024-04-15 16:51:37 -0500

    P3 start

--- chatbot/basic_chatbot.py
+++ chatbot/basic_chatbot.py
@@ -22,22 +22,10 @@ while True:
     if user_input.lower() == 'quit':
         print("--------------------------------------------------------------------------------")
         break
-        
-
-    # Add the user's message to the messages list
     messages.append(HumanMessage(content=user_input))
-
-    # Invoke the chat model with the current conversation context
     response = chat.invoke(messages)
-    
-    # Assuming the response is an AIMessage object, print its content
-    # Note: The actual handling might need adjustments based on the implementation of `chat.invoke`
     util.printWithColor("Bot: ", "green")
     print(response.content)
-
-    # Optionally, you can also append the AIMessage to messages if you want the bot's response
-    # to also be considered as part of the context for future messages. This depends on the
-    # implementation details of your ChatOpenAI class.
     messages.append(SystemMessage(content=response.content))
 
 chat.invoke(messages)
